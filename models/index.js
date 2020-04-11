916f6e93209af2f265b718dbdba52c1157e93f05
Ryan Kelley <rdkelley@users.noreply.github.com>
2020-04-11 14:12:01 -0700

    reset sequelize index and add env vars for db connection

--- models/index.js
+++ models/index.js
@@ -1,42 +1,39 @@
 "use strict";
 
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
+var basename = path.basename(module.filename);
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + "/../config/config.js")[env];
+var db = {};
 
-let config;
+console.log(config);
 
-console.log("env", env);
 
-if (env === "production") {
-  config = require(__dirname + "/../config/config-prod.js")[env];
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  config = require(__dirname + "/../config/config.json")[env];
+  var sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
-const db = {};
-
-let sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
-
 fs.readdirSync(__dirname)
-  .filter((file) => {
+  .filter(function (file) {
     return (
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
     );
   })
-  .forEach((file) => {
-    const model = sequelize["import"](path.join(__dirname, file));
+  .forEach(function (file) {
+    var model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
+Object.keys(db).forEach(function (modelName) {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
